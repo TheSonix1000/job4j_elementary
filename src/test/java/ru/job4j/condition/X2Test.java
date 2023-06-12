5f9b35523b4b42fe0417155ab995436ebda15a61
Sonix <Ivholod1@yandex.ru>
2023-06-12 15:31:07 +0500

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -17,12 +17,12 @@ public class X2Test {
     }
 
     @Test
-    public void whenMinusA1B1C1X1Then3() {
-        int a = -1;
+    public void whenMinusA1B1C1X1Then1() {
+        int a = 1;
         int b = 1;
         int c = 1;
         int x = 1;
-        int expected = 3;
+        int expected = 1;
         int rsl = X2.calc(-a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
@@ -39,19 +39,19 @@ public class X2Test {
     }
 
     @Test
-    public void whenMinusA1B1C0X1Then2() {
-        int a = -1;
+    public void whenMinusA1B1C0X1Then0() {
+        int a = 1;
         int b = 1;
         int c = 0;
         int x = 1;
-        int expected = 2;
+        int expected = 0;
         int rsl = X2.calc(-a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
 
     @Test
     public void whenMinusA1B1C1X0Then1() {
-        int a = -1;
+        int a = 1;
         int b = 1;
         int c = 1;
         int x = 0;
