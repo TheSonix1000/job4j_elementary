fc823af88d3b6e456b27e81fd610bd02b5ae5ef9
Sonix <Ivholod1@yandex.ru>
2023-06-12 13:33:09 +0500

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -17,35 +17,46 @@ public class X2Test {
     }
 
     @Test
-    public void whenAminus1B1C1X1Then1() {
+    public void whenMinusA1B1C1X1Then1() {
         int a = -1;
         int b = 1;
         int c = 1;
         int x = 1;
+        int expected = 3;
+        int rsl = X2.calc(-a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenMinusA0B1C1X1Then0() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(-a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenMinusA1B1C0X1Then0() {
+        int a = -1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(-a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenMinusA1B1C1X0Then0() {
+        int a = -1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
         int expected = 1;
-        int rsl = X2.calc(a, b, c, x);
-        Assert.assertEquals(expected, rsl);
-    }
-
-    @Test
-    public void whenAminus1B1C0X1Then0() {
-        int a = -1;
-        int b = 1;
-        int c = 0;
-        int x = 1;
-        int expected = 0;
-        int rsl = X2.calc(a, b, c, x);
-        Assert.assertEquals(expected, rsl);
-    }
-
-    @Test
-    public void whenAminus1B1C0X0Then0() {
-        int a = -1;
-        int b = 1;
-        int c = 0;
-        int x = 1;
-        int expected = 0;
-        int rsl = X2.calc(a, b, c, x);
+        int rsl = X2.calc(-a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
 }
